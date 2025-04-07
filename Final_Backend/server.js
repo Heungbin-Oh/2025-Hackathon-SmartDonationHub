047a161a872769341e7c3670a45cabbdfd8f6246
Heungbin-Oh <orane5050@gmail.com>
2025-04-07 09:15:02 -0400

    Update server.js

--- Final_Backend/server.js
+++ Final_Backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const connectToDatabase = require('./lib/db');
 const app = express();
 require('dotenv').config();
