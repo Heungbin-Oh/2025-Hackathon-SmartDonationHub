f3c2c3ba2ba57eecd1d76d1b237a5585bea0dde0
Heungbin-Oh <orane5050@gmail.com>
2025-04-06 20:47:08 -0400

    Update server.js

--- Final_Backend/server.js
+++ Final_Backend/server.js
@@ -14,13 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.SECRET_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
-console.log("Connecting to MongoDB with URI:", process.env.SECRET_URI);
+
 // Routes
 const homeRoutes = require('./Routes/home');
 const charityRoutes = require('./Routes/charityRoutes');
